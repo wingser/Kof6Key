79791110ea2d532eb98d2e58b08933a088e45761
wingser <wingser@foxmail.com>
2024-08-16 16:20:21 +0800

    初始化项目 拳皇键盘6键设置。支持方向1、3键设置。 可以用visual studio直接加载开发修改代码。

---
+++ Main.Designer.cs
@@ -0,0 +1,159 @@
+﻿using System.Windows.Forms;
+
+namespace Demo
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            radioGlobal = new RadioButton();
+            textBoxLog = new TextBox();
+            groupBox2 = new GroupBox();
+            label1 = new Label();
+            clearLogButton = new Button();
+            radioNone = new RadioButton();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // radioGlobal
+            // 
+            radioGlobal.AutoSize = true;
+            radioGlobal.BackColor = System.Drawing.Color.White;
+            radioGlobal.Checked = true;
+            radioGlobal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            radioGlobal.Location = new System.Drawing.Point(10, 34);
+            radioGlobal.Margin = new Padding(5, 4, 5, 4);
+            radioGlobal.Name = "radioGlobal";
+            radioGlobal.Size = new System.Drawing.Size(57, 21);
+            radioGlobal.TabIndex = 10;
+            radioGlobal.TabStop = true;
+            radioGlobal.Text = "启动";
+            radioGlobal.UseVisualStyleBackColor = false;
+            radioGlobal.CheckedChanged += radioGlobal_CheckedChanged;
+            // 
+            // textBoxLog
+            // 
+            textBoxLog.BackColor = System.Drawing.Color.White;
+            textBoxLog.BorderStyle = BorderStyle.FixedSingle;
+            textBoxLog.Dock = DockStyle.Fill;
+            textBoxLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBoxLog.Location = new System.Drawing.Point(0, 125);
+            textBoxLog.Margin = new Padding(5, 4, 5, 4);
+            textBoxLog.Multiline = true;
+            textBoxLog.Name = "textBoxLog";
+            textBoxLog.ReadOnly = true;
+            textBoxLog.ScrollBars = ScrollBars.Vertical;
+            textBoxLog.Size = new System.Drawing.Size(358, 0);
+            textBoxLog.TabIndex = 8;
+            textBoxLog.WordWrap = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = System.Drawing.Color.White;
+            groupBox2.Controls.Add(label1);
+            groupBox2.Controls.Add(clearLogButton);
+            groupBox2.Controls.Add(radioNone);
+            groupBox2.Controls.Add(radioGlobal);
+            groupBox2.Dock = DockStyle.Top;
+            groupBox2.FlatStyle = FlatStyle.Flat;
+            groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            groupBox2.Location = new System.Drawing.Point(0, 0);
+            groupBox2.Margin = new Padding(5, 4, 5, 4);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Padding = new Padding(5, 4, 5, 4);
+            groupBox2.Size = new System.Drawing.Size(358, 125);
+            groupBox2.TabIndex = 9;
+            groupBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = System.Drawing.Color.SeaShell;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Location = new System.Drawing.Point(153, 12);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(180, 70);
+            label1.TabIndex = 17;
+            label1.Text = "【→】：启动停止热键\r\n【Pause】: 显示隐藏程序\r\n启动后，增加1、3方向键；\r\nQ→1、E→3。";
+            // 
+            // clearLogButton
+            // 
+            clearLogButton.Location = new System.Drawing.Point(26, 87);
+            clearLogButton.Margin = new Padding(5, 4, 5, 4);
+            clearLogButton.Name = "clearLogButton";
+            clearLogButton.Size = new System.Drawing.Size(113, 38);
+            clearLogButton.TabIndex = 16;
+            clearLogButton.Text = "Clear Log";
+            clearLogButton.UseVisualStyleBackColor = true;
+            clearLogButton.Click += clearLog_Click;
+            // 
+            // radioNone
+            // 
+            radioNone.AutoSize = true;
+            radioNone.BackColor = System.Drawing.Color.White;
+            radioNone.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            radioNone.Location = new System.Drawing.Point(77, 34);
+            radioNone.Margin = new Padding(5, 4, 5, 4);
+            radioNone.Name = "radioNone";
+            radioNone.Size = new System.Drawing.Size(57, 21);
+            radioNone.TabIndex = 14;
+            radioNone.Text = "停止";
+            radioNone.UseVisualStyleBackColor = false;
+            radioNone.CheckedChanged += radioNone_CheckedChanged;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(358, 84);
+            Controls.Add(textBoxLog);
+            Controls.Add(groupBox2);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(5, 4, 5, 4);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Main";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Kof防冲突键盘设置";
+            Load += Main_Load;
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioGlobal;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioNone;
+        private System.Windows.Forms.Button clearLogButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,166 @@
+﻿// This code is distributed under MIT license. 
+// Copyright (c) 2015 George Mamaladze
+// See license.txt or https://mit-license.org/
+
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+
+namespace Demo
+{
+    public partial class Main : Form
+    {
+        private IKeyboardMouseEvents m_Events;
+        private bool bAutoFire = false;
+        private bool b6key = true;
+
+        public Main()
+        {
+            InitializeComponent();
+            radioGlobal.Checked = true;
+            SubscribeGlobal();
+            FormClosing += Main_Closing;
+        }
+
+        private void Main_Closing(object sender, CancelEventArgs e)
+        {
+            Unsubscribe();
+        }
+
+        private void SubscribeApplication()
+        {
+            Unsubscribe();
+            Subscribe(Hook.AppEvents());
+        }
+
+        private void SubscribeGlobal()
+        {
+            Unsubscribe();
+            Subscribe(Hook.GlobalEvents());
+        }
+
+        private void Subscribe(IKeyboardMouseEvents events)
+        {
+            m_Events = events;
+            m_Events.KeyDown += OnKeyDown;
+            m_Events.KeyUp += OnKeyUp;
+            m_Events.KeyPress += HookManager_KeyPress;
+        }
+
+        private void Unsubscribe()
+        {
+            if (m_Events == null) return;
+            m_Events.KeyDown -= OnKeyDown;
+            m_Events.KeyUp -= OnKeyUp;
+            m_Events.KeyPress -= HookManager_KeyPress;
+
+            m_Events.Dispose();
+            m_Events = null;
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            Log(string.Format("KeyDown  \t\t {0}\n", e.KeyCode));
+
+            // q代表键盘1方向，e代表键盘3方向
+            if (b6key)
+            {
+                 if (e.KeyCode == Keys.Q)
+                {
+                    SendKeys.SendWait("(as)");
+                }else if (e.KeyCode == Keys.E)
+                {
+                    SendKeys.SendWait("(ds)");
+                }
+            }
+            }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            Log(string.Format("KeyUp  \t\t\t {0}\n", e.KeyCode));
+
+            // 处理热键开关，当发现弹起→按键的时候，切换生效状态。
+            if (e.KeyCode == Keys.Right)
+            {
+                if (b6key)
+                {
+                    Log("关闭6键");
+                    radioGlobal.Checked = false;
+                    radioNone.Checked = true;
+                    b6key = false;
+                }
+                else
+                {
+                    Log("开启6键");
+                    radioGlobal.Checked = true;
+                    radioNone.Checked = false;
+                    b6key = true;
+                }
+            }
+
+            // pause热键改为显示隐藏程序
+            if (e.KeyCode == Keys.Pause)
+            {
+                if (this.Visible == true)
+                {
+                    this.Visible = false;
+                }
+                else
+                {
+                    this.Visible = true;
+                }
+            }
+        }
+
+        private void HookManager_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Log(string.Format("KeyPress \t\t\t {0}\n", e.KeyChar));
+            // 长按连击，只处理UI按键，并且开启了长按连击，因为模拟按键也有键盘事件，会有死循环。后面想办法处理
+            /*
+             * 
+            if(bAutoFire)
+            {
+                Thread.Sleep(10);
+                if (e.KeyChar == 'u')
+                { 
+                    SendKeys.SendWait("{u}");
+                }else if (e.KeyChar == 'i')
+                {
+                    SendKeys.SendWait("{i}");
+                }
+            }
+            */
+        }
+
+        private void Log(string text)
+        {
+            if (IsDisposed) return;
+            textBoxLog.AppendText(text);
+            textBoxLog.AppendText(Environment.NewLine);
+            textBoxLog.ScrollToCaret();
+        }
+
+        private void radioGlobal_CheckedChanged(object sender, EventArgs e)
+        {
+            //if (((RadioButton)sender).Checked) SubscribeGlobal();
+            if (((RadioButton)sender).Checked) b6key = true;
+        }
+
+        private void radioNone_CheckedChanged(object sender, EventArgs e)
+        {
+            // if (((RadioButton)sender).Checked) Unsubscribe();
+            if (((RadioButton)sender).Checked) b6key = false;
+        }
+
+        private void clearLog_Click(object sender, EventArgs e)
+        {
+            textBoxLog.Clear();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            SubscribeGlobal();
+        }
+    }
+}
